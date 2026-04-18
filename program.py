a1dd4f5deb850fb461f6d5c69a9f689f804e511f
DotRandomDot <57591549+DotRandomDot@users.noreply.github.com>
2026-04-18 19:26:35 +0200

    added several comments that explain the code

--- program.py
+++ program.py
@@ -9,17 +9,19 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# Creates a client that connects to the google gemini api
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-# A response will now be only sent when requested
+# Function sends a prompt to google gemini api to test if the api key is working correctly
 def test_gemini():
     response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents="Summarize this text..."
+        model = "gemini-2.0-flash",
+        contents = "Summarize this text..."
     )
     return response.text
 
-# Code For Fetching News
+# Function retrieves news articles from newspapi based on user provided search query
+# Requests are sent to newsapi endpoint and returns a list of matching articles
 def fetch_news(query):
     url = "https://newsapi.org/v2/everything"
     params = {
@@ -29,11 +31,15 @@ def fetch_news(query):
         "apiKey": os.getenv("NEWS_API_KEY")
     }
 
+    # requests.get sends a request to newsapi server
+    # server sends data back in JSON format
+    # data only extracts title and url of the article
     response = requests.get(url, params=params)
     data = response.json()
     return data["articles"]
 
-# API Endpoint
+# Function defines flask api endpoint - handles incoming requests
+# Reads query parameters, fetches articles using fetch_news() and returns JSON response
 @app.route("/news-summary")
 def news_summary():
     try:
@@ -42,11 +48,12 @@ def news_summary():
 
         articles = fetch_news(query)
 
-        # sends a response to frontend
+        # jsonify converts python data into a JSON response
+        # articles table extracts only the title and url of the article
         return jsonify({
             "articles": [
                 {"title": a["title"], "url": a["url"]}
-                for a in articles
+                for a in articles # loops through every article
             ],
         })
 
